1e75041fd69c519a354c020e87f1a012a565bc75
Thomas Whaley <twhaley1@my.westga.edu>
2020-03-05 20:32:07 -0500

    Added client and server projects.

---
+++ CS4225P1Group6-Server/src/edu/westga/cs4225/project1/group6/Main.java
@@ -0,0 +1,20 @@
+package edu.westga.cs4225.project1.group6;
+
+/**
+ * Starting point for the server.
+ * 
+ * @author Luke Whaley
+ *
+ */
+public class Main {
+
+	/**
+	 * Starting point for the server application.
+	 * 
+	 * @param args program arguments.
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
