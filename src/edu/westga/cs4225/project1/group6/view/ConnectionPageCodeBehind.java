1286c20dd48c2a620e97af3ef96f73abb93c3f98
Thomas Whaley <twhaley1@my.westga.edu>
2020-03-05 20:11:23 -0500

    Starter Project.

---
+++ src/edu/westga/cs4225/project1/group6/view/ConnectionPageCodeBehind.java
@@ -0,0 +1,38 @@
+package edu.westga.cs4225.project1.group6.view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * Code behind for ./fxml/ConnectionPage.fxml
+ * 
+ * @author Luke Whaley
+ *
+ */
+public class ConnectionPageCodeBehind {
+	 
+	@FXML
+	private AnchorPane pane;
+
+	@FXML
+	private TextField hostTextField;
+
+	@FXML
+	private TextField portTextField;
+
+	@FXML
+	private Button connectButton;
+
+	@FXML
+	private void initialize() {
+		
+	}
+	
+	@FXML
+	private void onConnectButtonAction(ActionEvent event) {
+
+	}
+}
