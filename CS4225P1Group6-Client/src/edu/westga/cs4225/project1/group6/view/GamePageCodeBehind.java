e4c412316adfe742d237bbdb71e3768653a17c16
Brandon Walker <brandoneugenewalker@outlook.com>
2020-03-07 23:05:29 -0500

    Added UI for the main game page.
    Cleaned up unwanted files and added gitignore.

---
+++ CS4225P1Group6-Client/src/edu/westga/cs4225/project1/group6/view/GamePageCodeBehind.java
@@ -0,0 +1,60 @@
+package edu.westga.cs4225.project1.group6.view;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * Handles displaying and interaction with the user playing the game.
+ * @author Luke Whaley, Brandon Walker, Kevin Flynn
+ *
+ */
+public class GamePageCodeBehind {
+
+    @FXML
+    private TextArea allyOneTextArea;
+
+    @FXML
+    private TextArea allyTwoTextArea;
+
+    @FXML
+    private TextArea allyThreeTextArea;
+
+    @FXML
+    private TextArea monsterStatusTextArea;
+
+    @FXML
+    private TextArea playerStatusTextArea;
+
+    @FXML
+    private TextArea battleLogTextArea;
+
+    @FXML
+    private Button secondaryActionButton;
+
+    @FXML
+    private Button primaryActionButton;
+
+    @FXML
+    private Button leaveGameButton;
+
+    @FXML
+    private TextArea roleDetailsTextArea;
+
+    @FXML
+    void leaveGameClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void primaryActionClicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void secondaryActionClicked(MouseEvent event) {
+
+    }
+
+}
+
