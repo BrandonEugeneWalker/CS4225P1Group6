1b8f742318d49edd5590656f3501efde44f9ad30
Brandon Walker <brandoneugenewalker@outlook.com>
2020-03-09 00:04:42 -0400

    Added skeletons for the client connection and server.

---
+++ CS4225P1Group6-Client/src/edu/westga/cs4225/project1/group6/client/RpgServerConnection.java
@@ -0,0 +1,10 @@
+package edu.westga.cs4225.project1.group6.client;
+
+/**
+ * Handles interactions with the server.
+ * @author Luke Whaley, Brandon Walker, Kevin Flynn
+ *
+ */
+public class RpgServerConnection {
+
+}
---
+++ CS4225P1Group6-Client/src/edu/westga/cs4225/project1/group6/controller/GamePageController.java
@@ -0,0 +1,10 @@
+package edu.westga.cs4225.project1.group6.controller;
+
+/**
+ * Handles interaction between the GamePage and the model logic.
+ * @author Luke Whaley, Brandon Walker, Kevin Flynn
+ *
+ */
+public class GamePageController {
+
+}
